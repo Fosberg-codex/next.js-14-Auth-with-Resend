cba07c360b398cc6ce813cf2514f44df59333bb5
Fosberg Addai <83887297+Fosberg-codex@users.noreply.github.com>
2024-08-17 23:05:47 +1200

    add links to text for reach out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-2 items-center justify-between p-24">
       <h1>Login authentication and reset password with Next.js 14 app router and Resend email api</h1>
-      <div>Please star git repo: <Link href="">github</Link> or follow me at <Link href="">@Fosberg Addai</Link></div>
+      <div>Please star git repo: <Link href="https://github.com/Fosberg-codex/next.js-14-with-Resend">github</Link> or follow me at <Link href="https://x.com/FosbergAddai">@Fosberg Addai</Link></div>
     </main>
   );
 }
